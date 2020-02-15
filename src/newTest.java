f34f13effd2a8e152af934f06bdf43eb562dbbff
yy <jfjf@gmail.com>
2020-02-15 10:38:13 +0330

    testMsg

---
+++ src/newTest.java
@@ -0,0 +1,9 @@
+public class newTest {
+    public static void main(String[] args)
+    {
+        if("hello" == "hello")
+            System.out.println("y");
+        else
+            System.out.println("n");
+    }
+}
